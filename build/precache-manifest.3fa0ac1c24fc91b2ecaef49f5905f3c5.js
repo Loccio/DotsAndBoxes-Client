a42be34e371321a9d86acec81d7c79e9da75a979
Loccio <56678670+Loccio@users.noreply.github.com>
2020-01-23 15:27:38 +0100

    deploy update 23-01-20

---
+++ build/precache-manifest.3fa0ac1c24fc91b2ecaef49f5905f3c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ecb68865fb0174b62c6c7344e2a9fda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96720c701bf6f0a8383e",
+    "url": "/static/css/main.51756151.chunk.css"
+  },
+  {
+    "revision": "e877e27f3260a929235f",
+    "url": "/static/js/2.7b6450da.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.7b6450da.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96720c701bf6f0a8383e",
+    "url": "/static/js/main.b8f48594.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
